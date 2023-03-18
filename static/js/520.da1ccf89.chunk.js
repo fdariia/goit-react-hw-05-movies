0bdb6ca099bddeb8eef61ee0e4ebf1656a697133
fdariia <117392356+fdariia@users.noreply.github.com>
2023-03-18 10:50:50 +0000

    Deploying to gh-pages from @ fdariia/goit-react-hw-05-movies@75c854ec746ea1f87c8254f8039a680dff902610 🚀

---
+++ static/js/520.da1ccf89.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[520],{520:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Not found page"})}}}]);
+//# sourceMappingURL=520.da1ccf89.chunk.js.map
