f48c834d2577b7c2f32ddaae08f1a4f3f44e84f0
fdariia <fdariya3005@gmail.com>
2023-03-18 12:48:42 +0200

    Create HomePage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { fetchTrending } from '../api/movies-api';
+import css from './HomePage.module.css';
+
+const HomePage = () => {
+  const [movies, setMovies] = useState([]);
+  const location = useLocation();
+
+  useEffect(() => {
+    async function getTrendingMovies() {
+      const response = await fetchTrending();
+      console.log(response);
+      setMovies(response);
+    }
+
+    getTrendingMovies();
+  }, []);
+
+  return (
+    <div className={css.homePage}>
+      <h1 className={css.title}>Trending today</h1>
+
+      {movies && (
+        <ul>
+          {movies.map(({ title, id }) => {
+            return (
+              <li className={css.homePageItem} key={id}>
+                <Link to={`movie/${id}`} state={{ from: location }}>
+                  {title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default HomePage;
